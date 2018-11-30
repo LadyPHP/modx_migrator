8e8375fb34751cb67fc2f0dc67c351797233a550
Колоскова Александра Игоревна <AKoloskova@synergy.local>
2018-11-30 18:18:03 +0300

    скрипт загрузки и костомизации пакета установки

---
+++ includes/modMigrateToDev.class.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AKoloskova
+ * Date: 12.09.2018
+ * Time: 13:24
+ */
+
+class modMigrateToDev
+{
+    /**
+     * @var modInstall $install Reference to the modInstall instance.
+     */
+    public $install = null;
+
+    function __construct(modInstall &$install)
+    {
+        $this->install =& $install;
+    }
+
+    public function migrate()
+    {
+        if (stripos($_SERVER['HTTP_HOST'], '.dev') !== false) {
+
+            $database_server = $this->install->settings->get('database_server');
+            $table_prefix = $this->install->settings->get('table_prefix');
+            $dbase = $this->install->settings->get('dbase');
+            $database_user = $this->install->settings->get('database_user');
+            $database_password = $this->install->settings->get('database_password');
+            $filessql = [
+                'structure' => MODX_SETUP_PATH . 'structure_db.sql',
+                'demo_content' => MODX_SETUP_PATH . 'demo_content.sql'
+            ];
+
+            foreach ($filessql as $query_type => $filesql) {
+                if (!file_exists($filesql)) echo 'не найден файл';
+                $sqldate = file_get_contents($filesql);
+                $sqldate = str_replace('mgrt_', $table_prefix, $sqldate);
+                if ($query_type == 'demo_content') $sqldate = str_replace('MGRT_HOST', $_SERVER['HTTP_HOST'], $sqldate);
+                if (is_writable($filesql) === false) @chmod($filesql, 0664);
+                file_put_contents($filesql, $sqldate);
+
+                $command = 'mysql -h' . $database_server . ' -u' . $database_user . ' -p' . $database_password . ' ' . $dbase . ' < ' . $filesql;
+                if (exec($command) === false) echo 'не возможно выполнить функцию exec()';
+            }
+        } else echo 'Установка таблиц из бекапов миграции не возможна, так как адрес сайта не содержит "dev".';
+    }
+}
---
+++ migrator.php
@@ -0,0 +1,162 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AKoloskova
+ * Date: 13.09.2018
+ * Time: 19:46
+ * Скачивает актуальную версию пакета с официального репозитория и добавляет изменения для решения вопроса миграции проектов на DEV
+ */
+
+// ссылка для определения версии
+$version_url = 'https://ilyaut.ru/download-modx/?api=getmodxlastversion';
+// массив со сслыками для скачивания
+$distibutive_urls = [
+    'https://ilyaut.ru/modx/modx-',
+    'https://modx.com/download/direct/modx-'
+];
+// имя сохраняемого архива дистрибутива
+$zip_name = 'modx.zip';
+// список файлов для модификации/добавления (пути относительно папки установщика - setup)
+$files_modify = [
+    'index.php',
+    'controllers/install.php',
+    //'includes/modMigrateToDev.class.php'
+];
+
+
+// Функция загрузки последней версии дистрибутива
+function downloadLastVersion($version_url, $urls, $file_path)
+{
+    // определяем название последней версии
+    $version = file_get_contents($version_url);
+
+    // проходим массив ссылок и пробуем скачать дистрибутив нужной версии
+    foreach ($urls as $url) {
+        // дополняем ссылку данными о версии и разширении скачиваемого файла
+        $url .= $version . '-pl-advanced.zip';
+
+        $fp = fopen($file_path, 'w+');
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        //curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_exec($ch);
+
+        curl_close($ch);
+        fclose($fp);
+
+        if (filesize($file_path) > 0) return $version;
+    }
+    // если мы не вышли из цикла выше, то значит не удалось скачать, поэтому возвращаем false
+    return false;
+}
+
+// Функция распаковки файлов дистрибутива из архива
+function unzip($file)
+{
+    global $amode;
+
+    if (!extension_loaded('zip')) {
+        if (strtoupper(substr(PHP_OS, 0, 3) == 'WIN')) {
+            if (!@dl('php_zip.dll')) return 0;
+        } else {
+            if (!@dl('zip.so')) return 0;
+        }
+    }
+
+    $zip = zip_open($file);
+    if ($zip) {
+        $old_umask = umask(0);
+        while ($zip_entry = zip_read($zip)) {
+            if (zip_entry_filesize($zip_entry) > 0) {
+                // str_replace must be used under windows to convert "/" into "\"
+                $complete_path = str_replace('/', '\\', dirname(zip_entry_name($zip_entry)));
+                $complete_name = str_replace('/', '\\', zip_entry_name($zip_entry));
+                $complete_name_arr = explode('\\', $complete_name);
+                $complete_name = str_replace($complete_name_arr[0] . '\\' . $complete_name_arr[1] . '\\', '', $complete_name);
+
+                if (!file_exists($complete_path) && strpos($complete_path, 'core') === false) {
+                    $tmp = '';
+                    foreach (explode('\\', $complete_path) as $i => $k) {
+                        if ($i < 2) continue;
+                        $tmp .= $k . '\\';
+                        if (!file_exists($tmp)) {
+                            @mkdir($tmp, $amode);
+                        }
+                    }
+                }
+                if (zip_entry_open($zip, $zip_entry, 'r')) {
+                    if (!file_exists($complete_name)) continue;
+                    $fd = fopen($complete_name, 'w');
+                    fwrite($fd, zip_entry_read($zip_entry, zip_entry_filesize($zip_entry)));
+                    fclose($fd);
+                    zip_entry_close($zip_entry);
+                }
+
+            }
+        }
+        umask($old_umask);
+        zip_close($zip);
+        return true;
+    }
+    zip_close($zip);
+}
+
+// 1. загрузка последней версии дистрибутива
+$result = downloadLastVersion($version_url, $distibutive_urls, $zip_name);
+if ($result === false) exit('Не удалось скачать архив дистрибутива последней версии. Проверьте настройки в файле миграции (ссылки для скачивания, пути и т.п.), а также на разрешения для запускаемого скрипта и места сохранения архива.');
+else $version = $result; // иначе если в качестве результата вернулась версия, то запишим ее в переменную
+
+if (!file_exists($zip_name)) exit('Не удалось открыть архив. Проверьте правильность его пути и имени, а также разрешения на папку.');
+
+
+// 2. распаковка файлов дистрибутива из архива
+$result = unzip($zip_name);
+if ($result === false) exit('Не удалось распаковать архив дистрибутива.');
+unlink($zip_name);
+
+// 3. модификация (костомизация) пакета
+// находим нужные файлы, которые будем модифицировать
+foreach ($files_modify as $file) {
+    if (!file_exists($file)) echo 'В новом дистрибутиве не найден файл ' . $file . '. Если это новый файл, который добавляем в дистрибутив, то можно проигнорировать данное сообщение. Иначе проверьте совместимость версий, обновите костомизацию и запустите повторно пересборку дистрибутива.';
+    else $file_content = file_get_contents($file);
+
+    $modify_content = '';
+
+    switch ($file) {
+        case 'index.php':
+            $modify_content = str_replace('<?php', '<?php
+if (stripos($_SERVER[\'HTTP_HOST\'], \'.dev\') === false) die(\'<html><head><title></title></head><body><h1>FATAL ERROR: MODX Migrate cannot continue in <span style="color:red">PRODACTION SERVER</span>! No-no-no</h1></body></html>\');
+                    ', $file_content);
+            break;
+        case 'controllers/install.php':
+            $modify_content = str_replace('return $parser->render(\'install.tpl\');', '
+// Импортируем файл базы данных
+if (!include(MODX_SETUP_PATH . \'includes/modMigrateToDev.class.php\')) {
+    die(\'<html><head><title></title></head><body><h1>FATAL ERROR: MODX Migrate cannot continue.</h1><p>Make sure you have uploaded all of the migrate/ files; your migrate/includes/modMigrateToDev.class.php file is missing.</p></body></html>\');
+}
+$migrate = new modMigrateToDev($install);
+var_dump($migrate);
+$migrate->migrate();
+// Конец импорта
+
+return $parser->render(\'install.tpl\');
+                    ', $file_content);
+            break;
+        default:
+            // просто скачиваем подготовленные наши файлы и размещаем их по нужным путям
+            // их нет в стандартном резозитории modx, поэтому не заморачиваемся
+            $modify_content = file_get_contents($file);
+    }
+
+    if (!empty($modify_content)) {
+        $file_handler = fopen($file, 'w');
+        fputs($file_handler, $modify_content);
+        fclose($file_handler);
+    }
+}
+
+unset($modify_content);
